b2abc417c7959b4b99dcaae9beebe0ce5c06735b
Thomas Nixon <tomjnixon@googlemail.com>
2010-08-05 22:00:28 +0100

    Fixed optimiser.

--- optimise.py
+++ optimise.py
@@ -1,12 +1,13 @@
 import itertools
 
 def optimise_transfers(values, person=lambda v: v[0], ammount=lambda v: v[1]):
+	assert(sum(map(ammount, values)) == 0)
 	transfers = list(_optimise_transfers(values, person, ammount))
-	for p in values:
-		assert((sum(t[2] for t in transfers if t[1] == p) 
-		        - sum(t[2]
-		              for t in transfers if t[0] == p))
-		       == ammount(p))
+#	for p in values:
+#		assert((sum(t[2] for t in transfers if t[1] == p) 
+#		        - sum(t[2]
+#		              for t in transfers if t[0] == p))
+#		       == ammount(p))
 	return transfers
 
 
@@ -39,10 +40,13 @@ def _optimise_transfers(values, person, ammount):
 	for group in find_transfer_groups(values, ammount):
 		transfers = find_simple_transfers(group, ammount)
 		if transfers is not None:
-			return transfers
+			for transfer in transfers:
+				yield transfer
 		else:
-			return min(map(make_transfers, itertools.permutations(group)),
-			           key=cost)
+			transfers = min(map(make_transfers, itertools.permutations(group)),
+			                key=cost)
+			for transfer in transfers:
+				yield transfer
 
 
 
